ad95d728f9ccf0d0e3f0ce71976c8f0db1134e24
K.Kiyokawa <koichi20110068@gmail.com>
2019-11-17 00:57:41 +0900

    :ambulance:fix date aggregation

--- src/application/DateAggregation.java
+++ src/application/DateAggregation.java
@@ -13,14 +13,14 @@ import javafx.collections.ObservableList;
 public class DateAggregation extends Aggregation<DateOfUse> {
 	@Override
 	public void aggregate(ObservableList<MoneyHistory> moneyHistories) {
-		Map<DateOfUse, Integer> date2priceSum = new TreeMap<>(); 
-		for (MoneyHistory history:moneyHistories) {
+		Map<DateOfUse, Integer> date2priceSum = new TreeMap<>();
+		for (MoneyHistory history : moneyHistories) {
 			DateOfUse key = history.getDate();
 			if (date2priceSum.containsKey(key)) {
 				int currentPrice = date2priceSum.get(key);
 				date2priceSum.replace(key, currentPrice + history.getPrice());
-			}else {
-				date2priceSum.put(key, 0);
+			} else {
+				date2priceSum.put(key, history.getPrice());
 			}
 		}
 
